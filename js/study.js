ac517ef60fbc74aa5404f116a00ddeff1cad61ca
Sasho878748 <kovalenko878748@gmail.com>
2023-03-01 21:31:15 +0200

    01.03.2023

---
+++ js/study.js
@@ -0,0 +1,8 @@
+// const a = 10;
+// console.log (a);
+// const message = 'JavaScript is awesome!';
+// alert(message);
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+// const hotelName = prompt('Please enter desired hotel name');
+// console.log(hotelName);
